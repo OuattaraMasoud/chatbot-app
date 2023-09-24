9417aefdfdc2068233dcd239c33587a7323a82b9
Mas'oud OUATTARA <masoud.ouattara@kreezus.com>
2023-09-24 21:52:34 +0000

    chat bot component

---
+++ src/app/chatbot/chatbot.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { OpenAiApiService } from '../service/open-ai-api.service.service';
+
+@Component({
+  selector: 'app-chatbot',
+  templateUrl: './chatbot.component.html',
+  styleUrls: ['./chatbot.component.css']
+})
+export class ChatbotComponent {
+  userMessage!: string;
+  assistantReply!: string;
+  chatMessages: { role: string, content: string }[] = [];
+
+  constructor(private openAiApiService: OpenAiApiService){}
+
+  sendMessage() {
+    console.error('method sendMessage called');
+    const userMessage = this.userMessage;
+    this.chatMessages.push({ role: 'user', content: userMessage });
+    this.openAiApiService.sendMessage(this.userMessage)
+      .subscribe(response => {
+        this.assistantReply = response.reply;
+        this.chatMessages.push({ role: 'assistant', content: this.assistantReply });
+        this.userMessage = '';
+      });
+  }
+}
